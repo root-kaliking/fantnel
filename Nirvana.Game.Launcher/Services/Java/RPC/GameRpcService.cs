34e922ba274ee8ec21316327e0325017b9cfd79e
风横 <3547694806@qq.com>
2026-05-21 19:04:39 +0800

    涅槃科技 [*]由于 ProcessPlayerSkin 仍存在问题，已关闭

--- Nirvana.Game.Launcher/Services/Java/RPC/GameRpcService.cs
+++ Nirvana.Game.Launcher/Services/Java/RPC/GameRpcService.cs
@@ -11,11 +11,7 @@ using Nirvana.Game.Launcher.Entities;
 using Nirvana.Game.Launcher.Services.Java.RPC.Events;
 using Nirvana.WPFLauncher.Entities.WPFLauncher.Launch.RPC;
 using Nirvana.WPFLauncher.Entities.WPFLauncher.Launch.Skin;
-using Nirvana.WPFLauncher.Entities.WPFLauncher.Minecraft;
-using Nirvana.WPFLauncher.Entities.WPFLauncher.NetGame.GameLaunch.Texture;
-using Nirvana.WPFLauncher.Protocol;
 using Nirvana.WPFLauncher.Utils;
-using NirvanaAPI.Utils;
 using Serilog;
 
 namespace Nirvana.Game.Launcher.Services.Java.RPC;
@@ -91,6 +87,7 @@ public class GameRpcService(int port, EntityLaunchGame launchGame, EnumGameVersi
             if (array != null) {
                 SendControlData(array);
             }
+
             Log.Information("[RPC] Sent new-version authentication to {0}:{1} | User: {2} | Role: {3} | Protocol: {4}", launchGame.ServerIp, launchGame.ServerPort, launchGame.Account.UserId, launchGame.RoleName, gameVersion);
         }
     }
@@ -128,53 +125,53 @@ public class GameRpcService(int port, EntityLaunchGame launchGame, EnumGameVersi
         Task.Run(() => ProcessPlayerSkin(entityOtherEnterWorldMsg));
     }
 
-private async Task ProcessPlayerSkin(EntityOtherEnterWorldMsg msg)
-	{
-        var list = await NPFLauncher.GetSkinListInGameAAsync(new EntityUserGameTextureRequest {
-            UserId = _skip32Cipher.ComputeUserIdFromUuid(msg.Uuid).ToString(),
-            ClientType = EnumGameClientType.Java
-        });
-		var filePath = string.Empty;
-		var skinMode = EnumSkinMode.Default;
-		if (list != null) {
-			foreach (var item in list.Where(s => s.SkinId.Length > 5)) {
-				skinMode = (EnumSkinMode)item.SkinMode;
-				var tempPath = Path.Combine(_dirSkinPath, "skin_" + item.SkinId + ".png");
-				if (File.Exists(tempPath) && FileUtil.IsFileReadable(tempPath)) {
-					filePath = tempPath;
-					break;
-				}
-				try {
-                    var entity = await NPFLauncher.GetNetGameComponentDownloadListAAsync(item.SkinId);
-					var text = entity?.SubEntities.Select(sub => sub.ResUrl).FirstOrDefault();
-					if (text != null)
-					{
-						Directory.CreateDirectory(_dirSkinPath);
-                        var bytes = await _httpClient.GetByteArrayAsync(text);
-						await FileUtil.WriteFileSafelyAsync(tempPath, bytes);
-						if (File.Exists(tempPath) && FileUtil.IsFileReadable(tempPath)) {
-							filePath = tempPath;
-							break;
-						}
-					}
-				} catch (Exception exception) {
-					Log.Error(exception, "[RPC] Failed to handle skin for player {Name}", msg.Name);
-					try {
-						if (File.Exists(tempPath)) {
-							File.Delete(tempPath);
-						}
-					} catch {
-						Log.Error(exception, "[RPC] Failed to delete temp file {Path}", filePath);
-					}
-				}
-			}
-		}
-		Log.Information("[RPC] Sending skin data for {0}: {1}", msg.Name, filePath);
-		var array = SimplePack.Pack((ushort)520, msg.Name, filePath, string.Empty, skinMode);
-		if (array != null) {
-			SendControlData(array);
-		}
-	}
+    private void ProcessPlayerSkin(EntityOtherEnterWorldMsg msg)
+    {
+        // var list = await NPFLauncher.GetSkinListInGameAAsync(new EntityUserGameTextureRequest {
+        //     UserId = _skip32Cipher.ComputeUserIdFromUuid(msg.Uuid).ToString(),
+        //     ClientType = EnumGameClientType.Java
+        // });
+        var filePath = string.Empty;
+        var skinMode = EnumSkinMode.Default;
+        // if (list != null) {
+        // 	foreach (var item in list.Where(s => s.SkinId.Length > 5)) {
+        // 		skinMode = (EnumSkinMode)item.SkinMode;
+        // 		var tempPath = Path.Combine(_dirSkinPath, "skin_" + item.SkinId + ".png");
+        // 		if (File.Exists(tempPath) && FileUtil.IsFileReadable(tempPath)) {
+        // 			filePath = tempPath;
+        // 			break;
+        // 		}
+        // 		try {
+        //                   var entity = await NPFLauncher.GetNetGameComponentDownloadListAAsync(item.SkinId);
+        // 			var text = entity?.SubEntities.Select(sub => sub.ResUrl).FirstOrDefault();
+        // 			if (text != null)
+        // 			{
+        // 				Directory.CreateDirectory(_dirSkinPath);
+        //                       var bytes = await _httpClient.GetByteArrayAsync(text);
+        // 				await FileUtil.WriteFileSafelyAsync(tempPath, bytes);
+        // 				if (File.Exists(tempPath) && FileUtil.IsFileReadable(tempPath)) {
+        // 					filePath = tempPath;
+        // 					break;
+        // 				}
+        // 			}
+        // 		} catch (Exception exception) {
+        // 			Log.Error(exception, "[RPC] Failed to handle skin for player {Name}", msg.Name);
+        // 			try {
+        // 				if (File.Exists(tempPath)) {
+        // 					File.Delete(tempPath);
+        // 				}
+        // 			} catch {
+        // 				Log.Error(exception, "[RPC] Failed to delete temp file {Path}", filePath);
+        // 			}
+        // 		}
+        // 	}
+        // }
+        Log.Information("[RPC] Sending skin data for {0}: {1}", msg.Name, filePath);
+        var array = SimplePack.Pack((ushort)520, msg.Name, filePath, string.Empty, skinMode);
+        if (array != null) {
+            SendControlData(array);
+        }
+    }
 
     private static void HandleLoginGame(byte[] data)
     {
@@ -232,6 +229,7 @@ private async Task ProcessPlayerSkin(EntityOtherEnterWorldMsg msg)
             _sendCache.Add(message);
             return;
         }
+
         var buffer = BitConverter.GetBytes((ushort)message.Length).Concat(message).ToArray();
         try {
             _writer?.Write(buffer);
